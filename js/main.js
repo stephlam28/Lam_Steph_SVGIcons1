4f7feec836ac22e2a7cf4150077ef18d9ea373c9
stephlam28 <lam.steph28@gmail.com>
2023-06-02 14:19:10 -0400

    Reuploading Files
    New repo as previous SVGIcons repo received "HEAD Detached" message

---
+++ js/main.js
@@ -0,0 +1,37 @@
+console.log("JS file connected");
+
+//blueberries
+const getEventObject = (blueberries) => {
+    console.log("You are selecting Ocean Blueberry");
+}
+blueberries.addEventListener("click", getEventObject);
+
+//cherry
+const getEventObject1 = (cherry) => {
+    console.log("You are selecting Red Cherry");
+}
+cherry.addEventListener("click", getEventObject1);
+
+//lemon
+const getEventObject2 = (lemon) => {
+    console.log("You are selecting Original Lemonade");
+}
+lemon.addEventListener("click", getEventObject2);
+
+//peach
+const getEventObject3 = (peach) => {
+    console.log("You are selecting Peach Fizz");
+}
+peach.addEventListener("click", getEventObject3);
+
+//raspberry
+const getEventObject4 = (raspberry) => {
+    console.log("You are selecting Raspberry Fizz");
+}
+raspberry.addEventListener("click", getEventObject4);
+
+//strawberry
+const getEventObject5 = (strawberry) => {
+    console.log("You are selecting Strawberry Fizz");
+}
+strawberry.addEventListener("click", getEventObject5);
